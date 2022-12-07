00f2ffbb126f179acc54b4b6bda82c1da80c82ee
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-12-07 11:38:14 +0800

    bfs init

--- main.c
+++ main.c
@@ -1,8 +1,106 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <malloc.h>
 
 #define nVertex 10
+
+#define SIZE 40
+
+struct queue {
+  int items[SIZE];
+  int front;
+  int rear;
+};
+
+struct queue* createQueue();
+void enqueue(struct queue* q, int);
+int dequeue(struct queue* q);
+void display(struct queue* q);
+int isEmpty(struct queue* q);
+void printQueue(struct queue* q);
+
+struct queue* createQueue() {
+  struct queue* q = malloc(sizeof(struct queue));
+  q->front = -1;
+  q->rear = -1;
+  return q;
+}
+
+int isEmpty(struct queue* q) {
+  if (q->rear == -1)
+    return 1;
+  else
+    return 0;
+}
+
+void enqueue(struct queue* q, int value) {
+  if (q->rear == SIZE - 1) {
+    printf("\nQueue is Full!!");
+  } else {
+    if (q->front == -1)
+      q->front = 0;
+    q->rear++;
+    q->items[q->rear] = value;
+  }
+}
+
+int dequeue(struct queue* q) {
+  int item;
+  if (isEmpty(q)) {
+    printf("Queue is empty");
+    item = -1;
+  } else {
+    item = q->items[q->front];
+    q->front++;
+    if (q->front > q->rear) {
+      printf("Resetting queue ");
+      q->front = q->rear = -1;
+    }
+  }
+  return item;
+}
+
+void printQueue(struct queue* q) {
+  int i;
+
+  if (isEmpty(q)) {
+    printf("Queue is empty");
+  } else {
+    printf("\nQueue contains \n");
+    for (i = q->front; i < q->rear + 1; i++) {
+      printf("%d ", q->items[i]);
+    }
+  }
+}
+
+void bfs(int graph[nVertex][nVertex], int startVertex) {
+  bool visited[nVertex];
+  for (int i = 0; i < nVertex; ++i) {
+    visited[i] = false;
+  }
+
+  struct queue* q = createQueue();
+  visited[startVertex] = true;
+  enqueue(q, startVertex);
+
+  while (!isEmpty(q)) {
+    printQueue(q);
+    int me = dequeue(q);
+    printf("Visited %d\n", me);
+
+    for (int ngb = 0; ngb < nVertex; ++ngb) {
+      if (0 == graph[me][ngb]) {
+        continue;
+      }
+
+      if (visited[ngb] == false) {
+        visited[ngb] = true;
+        enqueue(q, ngb);
+      }
+    }
+  }
+}
 
 int minDistance(const int dist[], const bool sptSet[]) {
   int min = INT_MAX;
@@ -84,6 +182,7 @@ int main() {
   };
 
   dijkstra(edgeWeights, 0);
+  bfs(edgeWeights, 0);
 
   return 0;
 }
