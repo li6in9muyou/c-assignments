02bf8b4bc7598182346ec78a86cc27cc6af67b53
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-12-16 10:36:08 +0800

    feat: prim's algorithm

--- main.c
+++ main.c
@@ -245,8 +245,9 @@ void dijkstra(int graph[nVertex][nVertex], int src) {
   printSolution(dist, previousStop, graph);
 }
 
+char vertexLable[nVertex] = "BCDFHLWXYZ";
+
 int main() {
-  char vertexLable[nVertex] = "BCDFHLWXYZ";
   int edgeWeights[nVertex][nVertex] = {
       {0, 0, 5, 0, 0, 4, 5, 0, 0, 8,},
       {0, 0, 0, 7, 6, 0, 0, 0, 6, 0,},
@@ -274,5 +275,34 @@ void kruskal(int edges[nVertex][nVertex]) {
 }
 
 void prim(int edges[nVertex][nVertex]) {
+  int edgeCnt = 0;
+  int treeWeight = 0;
+  int vertexIsSelected[nVertex] = {false,};
 
+  vertexIsSelected[nVertex - 1] = true;
+
+  while (edgeCnt < nVertex - 1) {
+    int min = INT_MAX;
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < nVertex; ++i) {
+      if (vertexIsSelected[i]) {
+        for (int j = 0; j < nVertex; ++j) {
+          if (!vertexIsSelected[j] && edges[i][j]) {
+            if (min > edges[i][j]) {
+              min = edges[i][j];
+              x = i;
+              y = j;
+            }
+          }
+        }
+      }
+    }
+    treeWeight += edges[x][y];
+    printf("%c -(%d)- %c\n", vertexLable[x], edges[x][y], vertexLable[y]);
+
+    vertexIsSelected[y] = true;
+    edgeCnt += 1;
+  }
+  printf("tree weight: %d\n", treeWeight);
 }
