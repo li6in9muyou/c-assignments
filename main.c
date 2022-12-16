ae31e479e9bba05791cb8703149ab3246acf0766
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-12-16 16:50:46 +0800

    refactor: avoid hard coding the number of vertices

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 void prim(int edges[nVertex][nVertex]);
 void kruskal(int weights[nVertex][nVertex]);
 
-void show_edge(int adjacentMatrix[10][10], int x, int y);
+void show_edge(int adjacentMatrix[nVertex][nVertex], int x, int y);
 
 int main() {
   int edgeWeights[nVertex][nVertex] = {
@@ -191,7 +191,7 @@ void prim(int weights[nVertex][nVertex]) {
   printf("tree weight: %d\n", treeWeight);
 }
 
-void show_edge(int adjacentMatrix[10][10], int x, int y) {
+void show_edge(int adjacentMatrix[nVertex][nVertex], int x, int y) {
   int smaller = min(x, y);
   int bigger = max(x, y);
 
