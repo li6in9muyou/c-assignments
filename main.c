d12a992961bdc4488aea80ee2408503fe578c5a1
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-12-16 14:45:18 +0800

    feat: kruskal's algorithm

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <stdlib.h>
 
 #define nVertex 10
 
@@ -270,8 +271,70 @@ int main() {
   return 0;
 }
 
-void kruskal(int edges[nVertex][nVertex]) {
+struct Edge {
+  int one;
+  int two;
+  int weight;
+};
 
+typedef struct Edge Edge;
+
+int do_find(const int parent[nVertex], int i) {
+  while (parent[i] != i) {
+    i = parent[i];
+  }
+  return i;
+}
+
+void do_union(int parent[nVertex], int i, int j) {
+  int a = do_find(parent, i);
+  int b = do_find(parent, j);
+  parent[a] = b;
+}
+
+int weight_ascending(const void* _a, const void* _b) {
+  Edge* a = (Edge*) _a;
+  Edge* b = (Edge*) _b;
+  return a->weight - b->weight;
+}
+
+void kruskal(int weights[nVertex][nVertex]) {
+  int edgeCnt = 0;
+  Edge edges[nVertex * nVertex];
+  for (int from = 0; from < nVertex; ++from) {
+    for (int to = 0; to < nVertex; ++to) {
+      bool isAdjacent = weights[from][to] != 0;
+      if (isAdjacent) {
+        edges[edgeCnt].weight = weights[from][to];
+        edges[edgeCnt].one = from;
+        edges[edgeCnt].two = to;
+        edgeCnt += 1;
+      }
+    }
+  }
+
+  qsort(
+      edges,
+      edgeCnt,
+      sizeof(Edge),
+      weight_ascending
+  );
+
+  int u[nVertex];
+  for (int i = 0; i < nVertex; ++i) {
+    u[i] = i;
+  }
+
+  int treeWeight = 0;
+  for (int i = 0; i < edgeCnt; ++i) {
+    Edge e = edges[i];
+    if (do_find(u, e.one) != do_find(u, e.two)) {
+      do_union(u, e.one, e.two);
+      printf("%c -(%d)- %c\n", vertexLable[e.one], e.weight, vertexLable[e.two]);
+      treeWeight += e.weight;
+    }
+  }
+  printf("tree weight: %d\n", treeWeight);
 }
 
 void prim(int edges[nVertex][nVertex]) {
